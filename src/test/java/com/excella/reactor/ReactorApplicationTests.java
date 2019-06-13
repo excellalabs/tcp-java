8c1aa9692d5d3336a8062c0c8205406d1f63c55b
James M. Stricker <jmstricker93@gmail.com>
2019-06-13 12:37:35 -0400

    remove intentionally-failing tests

--- src/test/java/com/excella/reactor/ReactorApplicationTests.java
+++ src/test/java/com/excella/reactor/ReactorApplicationTests.java
@@ -2,16 +2,10 @@ package com.excella.reactor;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertTrue;
-
 @Test
 public class ReactorApplicationTests {
 
   @Test
   public void contextLoads() {}
 
-  @Test
-  public void pleasefail(){
-    assertTrue(false);
-  }
 }
--- src/test/java/com/excella/reactor/controllers/EmployeeControllerTests.java
+++ src/test/java/com/excella/reactor/controllers/EmployeeControllerTests.java
@@ -2,14 +2,7 @@ package com.excella.reactor.controllers;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 public class EmployeeControllerTests {
     @Test
     public void contextLoads() {}
-
-    @Test
-    public void failplease(){
-        fail();
-    }
 }
