be071f42649f217db32081bc0df6ce0878cc169c
Kenneth Russell <kenrusselldc@gmail.com>
2019-06-26 16:51:17 -0400

    JA34 (#23)
    * JA-34
    
    * CORS fix
    
    * CORS fix
    
    * more fixes
    
    * CORS
    
    * CORS
    
    * CORS fix
    
    * CORS fix
    
    * MORE CORS OMG

--- src/main/java/com/excella/reactor/config/CorsFilter.java
+++ src/main/java/com/excella/reactor/config/CorsFilter.java
@@ -7,11 +7,15 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @Slf4j
 public class CorsFilter implements Filter {
 
@@ -24,10 +28,19 @@ public class CorsFilter implements Filter {
   public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
       throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) resp;
+    HttpServletRequest request = (HttpServletRequest) req;
     response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-    response.setHeader("Access-Control-Max-Age", "0");
-    chain.doFilter(req, resp);
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET, HEAD, PUT, OPTIONS, DELETE");
+    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader(
+        "Access-Control-Allow-Headers",
+        "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      chain.doFilter(req, resp);
+    }
   }
 
   @Override
