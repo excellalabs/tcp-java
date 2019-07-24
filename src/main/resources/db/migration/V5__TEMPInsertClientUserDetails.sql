05eab6e63bd2e01e222da377b016a0e0ef6445bb
Brendon Caulkins <32872899+brendoncaulkins@users.noreply.github.com>
2019-07-24 12:59:03 -0400

    ja25: Add Admin user, tie initial dummy users to dummy data (#39)
    * JA-25: Adding Email to User
    
    * goJF
    
    * removal of comments
    
    * ja25: Add Admin user, tie initial dummy users to dummy data

--- src/main/resources/db/migration/V5__TEMPInsertClientUserDetails.sql
+++ src/main/resources/db/migration/V5__TEMPInsertClientUserDetails.sql
@@ -2,5 +2,10 @@
 
 INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, authorities, access_token_validity)
 VALUES ('app', '{bcrypt}$2a$10$p85zHPktuZ.jd/BC/ZAGmeWiJIO0lvAQI0T5REavmA7XavdA9J5O.', 'read,write', 'password,refresh_token,client_credentials', 'ROLE_CLIENT', 300);
-INSERT INTO users (id, email, username, password, enabled) VALUES (1, 'test@excella.com', 'user', '{bcrypt}$2a$10$cyf5NfobcruKQ8XGjUJkEegr9ZWFqaea6vjpXWEaSqTa2xL9wjgQC', true);
+INSERT INTO users (id, email, username, password, enabled) VALUES (1, 'john@winchester.com', 'user', '{bcrypt}$2a$10$cyf5NfobcruKQ8XGjUJkEegr9ZWFqaea6vjpXWEaSqTa2xL9wjgQC', TRUE);
 INSERT INTO authorities (user_id, username, authority) VALUES (1, 'user', 'ROLE_USER');
+INSERT INTO users (id, email, username, password, enabled) VALUES (2, 'leslie.knope@in.parks.gov', 'admin', '{bcrypt}$2a$10$cyf5NfobcruKQ8XGjUJkEegr9ZWFqaea6vjpXWEaSqTa2xL9wjgQC', TRUE);
+INSERT INTO authorities (user_id, username, authority) VALUES (2, 'admin', 'ROLE_ADMIN');
+INSERT INTO users (id, email, username, password, enabled) VALUES 3, 'test@excella.com', 'user', '{bcrypt}$2a$10$cyf5NfobcruKQ8XGjUJkEegr9ZWFqaea6vjpXWEaSqTa2xL9wjgQC', true);
+INSERT INTO authorities (user_id, username, authority) VALUES (1, 'user', 'ROLE_USER');
+
