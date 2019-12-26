dbb3cedcc0b82d8e951c1e9ca012d6c8b080a6bf
ephultman <mister.parke@gmail.com>
2019-12-26 10:32:43 -0500

    RDS time

--- tcp-java-ocp/main.tf
+++ tcp-java-ocp/main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
   region                  = "us-east-2"
 }
+
+resource "aws_db_instance" "sampledb" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "11.5"
+  storage_type         = "gp2"
+  identifier_prefix    = "tcp-ocp-"
+  instance_class       = "db.t2.micro"
+  name                 = "app"
+  username             = "foo"
+  password             = "changeme"
+}
